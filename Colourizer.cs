b85429d6a4e4bfdfa4932a8f7dcd49d18321b96b
JoshMD24 <44683526+JoshMD24@users.noreply.github.com>
2020-12-01 13:24:41 -0500

    Add files via upload

---
+++ Colourizer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CustomWindow : EditorWindow
+{
+
+    // Variables
+    Color color;
+
+    [MenuItem("Tools/Colorizer")]
+    public static void ShowWindow()
+    {
+        GetWindow<CustomWindow>("Colorizer");
+    }
+
+    // Window display items
+    private void OnGUI()
+    {
+        GUILayout.Label("Color the selected objects", EditorStyles.boldLabel);
+
+        color = EditorGUILayout.ColorField("Color", color);
+
+        if (GUILayout.Button("Colorize"))
+            Colorizer();
+    }
+
+    // Methods
+    void Colorizer()
+    {
+
+        foreach (GameObject g in Selection.gameObjects)
+        {
+            Renderer ren = g.GetComponent<Renderer>();
+            if (ren)
+                ren.sharedMaterial.color = color;
+        }
+
+    }
+}
